e11f4bbe710fe385309eec7dfd739e15a117d956
Leonardo Araujo <leonardoaraujo.oct@gmail.com>
2022-09-28 20:21:08 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Mi primera descripcion"
+},
+{
+    id: 1,
+    title: "Mi primer tarea",
+    description: "Mi primera descripcion"
+},
+{
+    id: 2,
+    title: "Mi primer tarea",
+    description: "Mi primera descripcion"
+}
+]
